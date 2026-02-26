9a5b130e7beb3d2b228c6fbeb00588477341c648
Maria Eduarda Silva de Andrade Braz <art0066347@aluno.firjansenaisesi.com.br>
2026-02-26 13:30:52 -0300

    instalei o portal-noticias

---
+++ portal-noticias/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+import './App.css'
+import Rodape from './assets/componentes/home'
+import Menu from './assets/componentes/menu'
+import Home from './assets/componentes/home'
+
+function App() {
+ 
+
+  return (
+    <>
+  <Menu/>
+  <Home/>
+  <Rodape/>
+
+     
+    </>
+  )
+}
+
+export default App
---
+++ portal-noticias/src/assets/componentes/rodape.jsx
@@ -0,0 +1,14 @@
+function Rodape() {
+ 
+
+    return (
+      <>
+    <h1>  Rodape</h1>
+       
+      </>
+    )
+  }
+  
+  export default Rodape
+
+  
